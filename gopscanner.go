c8a61bb1e86e6e3207020c2f10d90a15968b923e
Andrew <62677201+Red-Darkin@users.noreply.github.com>
2024-05-22 10:27:06 -0500

    Update gopscanner.go
    Improve handling errors

--- gopscanner.go
+++ gopscanner.go
@@ -19,7 +19,7 @@ func scanPort(host string, port int, wg *sync.WaitGroup) {
     target := fmt.Sprintf("%s:%d", host, port)
     conn, err := net.DialTimeout("tcp", target, SYN_TIMEOUT)
     if err != nil {
-        return // Puerto cerrado o filtrado
+        return 
     }
     conn.Close()
     fmt.Printf("[*] Puerto %d abierto en %s\n", port, host)
@@ -38,20 +38,20 @@ func portScan(host string, startPort, endPort int) {
 
 func main() {
     if len(os.Args) < 4 {
-        fmt.Println("Uso: ./portscanner <host> <inicio_puerto> <fin_puerto>")
+        fmt.Println("Uso: ./portscanner <host> <start_port> <end_port>")
         return
     }
 
     host := os.Args[1]
     startPort, err := strconv.Atoi(os.Args[2])
     if err != nil {
-        fmt.Println("Error: Inicio de puerto no válido")
+        fmt.Println("Error: Invalid startPort")
         return
     }
 
     endPort, err := strconv.Atoi(os.Args[3])
     if err != nil {
-        fmt.Println("Error: Fin de puerto no válido")
+        fmt.Println("Error: Invalid endPort")
         return
     }
 
