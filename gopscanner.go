62ea574d98fd2697da2cc67203cdee946bcd7ccf
Andrew <62677201+Red-Darkin@users.noreply.github.com>
2024-05-22 15:06:57 -0500

    Update gopscanner.go
    Ability to read a file with hosts
    Flexibility to add ports to scan

--- gopscanner.go
+++ gopscanner.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+    "bufio"
+    "flag"
     "fmt"
     "net"
     "os"
     "strconv"
+    "strings"
     "sync"
     "time"
 )
@@ -19,16 +22,16 @@ func scanPort(host string, port int, wg *sync.WaitGroup) {
     target := fmt.Sprintf("%s:%d", host, port)
     conn, err := net.DialTimeout("tcp", target, SYN_TIMEOUT)
     if err != nil {
-        return 
+        return
     }
     conn.Close()
     fmt.Printf("[*] Open Port %s:%d\n", host, port)
 }
 
-func portScan(host string, startPort, endPort int) {
+func portScan(host string, ports []int) {
     var wg sync.WaitGroup
 
-    for port := startPort; port <= endPort; port++ {
+    for _, port := range ports {
         wg.Add(1)
         go scanPort(host, port, &wg)
     }
@@ -36,24 +39,79 @@ func portScan(host string, startPort, endPort int) {
     wg.Wait()
 }
 
+func scanFromFile(fileName string, ports []int) {
+    file, err := os.Open(fileName)
+    if err != nil {
+        fmt.Printf("Error opening file: %v\n", err)
+        return
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        host := scanner.Text()
+        portScan(host, ports)
+    }
+
+    if err := scanner.Err(); err != nil {
+        fmt.Printf("Error reading file: %v\n", err)
+    }
+}
+
+func parsePorts(ports string) ([]int, error) {
+    var result []int
+    parts := strings.Split(ports, ",")
+    for _, part := range parts {
+        if strings.Contains(part, "-") {
+            rangeParts := strings.Split(part, "-")
+            if len(rangeParts) != 2 {
+                return nil, fmt.Errorf("invalid range: %s", part)
+            }
+            start, err := strconv.Atoi(rangeParts[0])
+            if err != nil {
+                return nil, fmt.Errorf("invalid port: %s", rangeParts[0])
+            }
+            end, err := strconv.Atoi(rangeParts[1])
+            if err != nil {
+                return nil, fmt.Errorf("invalid port: %s", rangeParts[1])
+            }
+            for i := start; i <= end; i++ {
+                result = append(result, i)
+            }
+        } else {
+            port, err := strconv.Atoi(part)
+            if err != nil {
+                return nil, fmt.Errorf("invalid port: %s", part)
+            }
+            result = append(result, port)
+        }
+    }
+    return result, nil
+}
+
 func main() {
-    if len(os.Args) < 4 {
-        fmt.Println("Usage: ./portscanner <host> <start_port> <end_port>")
+    host := flag.String("h", "", "Host to scan")
+    fileName := flag.String("f", "", "File with hosts to scan")
+    portsFlag := flag.String("p", "", "Ports to scan (e.g., 80,443 or 1-1000)")
+
+    flag.Parse()
+
+    if *portsFlag == "" {
+        fmt.Println("Usage: ./portscanner -h <host> -p <ports> or ./portscanner -f <filename> -p <ports>")
         return
     }
 
-    host := os.Args[1]
-    startPort, err := strconv.Atoi(os.Args[2])
+    ports, err := parsePorts(*portsFlag)
     if err != nil {
-        fmt.Println("Error: Invalid startPort")
+        fmt.Printf("Error parsing ports: %v\n", err)
         return
     }
 
-    endPort, err := strconv.Atoi(os.Args[3])
-    if err != nil {
-        fmt.Println("Error: Invalid endPort")
-        return
+    if *fileName != "" {
+        scanFromFile(*fileName, ports)
+    } else if *host != "" {
+        portScan(*host, ports)
+    } else {
+        fmt.Println("Usage: ./portscanner -h <host> -p <ports> or ./portscanner -f <filename> -p <ports>")
     }
-
-    portScan(host, startPort, endPort)
 }
